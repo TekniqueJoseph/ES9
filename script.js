dd0563595e56e235d4b8ac6571de3e0d2804823c
TekniqueJoseph <joejacksun@gmail.com>
2023-04-10 10:48:23 -0400

    cleaned up js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// Object spread operator
-restParam(1, 2, 3, 4, 5);
-function restParam(p1, p2, ...p3) {
-  p1 = 1
-  p2 = 2
-  p3 = [3, 4, 5]
-}
-
-// const myObject = {
-//   a: 1,
-//   b: 2,
-//   c: 3
-// };
-
-// const { a, ...x } = myObject;
-// // a = 1
-// // x = { b: 2, c: 3 }
-
-// //finally
+//finally
 const urlz = [
   'https://swapi.py4e.com/api/people/1',
   'https://swapi.py4e.com/api/people/2',
